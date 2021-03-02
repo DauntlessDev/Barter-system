0dcfb7dc9526837a65a099108f9d39f4dca9cd6c
Hezzz <49769113+Hezzz@users.noreply.github.com>
2021-03-03 00:47:33 +0800

    Created Migration file for Item

---
+++ app/Database/Migrations/2021-03-02-104630_Item.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Item extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'item_id'               => [
+                'type'              => 'INT',
+                'constraint'        => 10,
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'poster_uid'            => [
+                'type'              => 'INT',
+                'constraint'        => 10,
+                'unsigned'          => true,
+            ],
+            'item_name'             => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '255',
+                'default'           => 'Unknown item',
+            ],
+            'photo_url'             => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '255',
+                'default'           => '#',     /* Please set default photo file */
+            ],
+            'avail_status'          => [
+                'type'              => 'ENUM',
+                'constraint'        => ['available', 'pending', 'unavailable'],
+                'default'           => 'available',
+            ],
+            'desc_title'            => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '255',
+                'default'           => 'Add a description title',
+            ],
+            'desc_content'          => [
+                'type'              => 'TEXT',
+                'null'              => true,
+            ],
+            'rating'                => [
+                'type'              => 'DECIMAL',
+                'constraint'        => '2,2',
+                'default'           => '0.0',
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('item_id');
+        $this->forge->addForeignKey('poster_uid', 'user', 'user_id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('item');
+}
+
+    public function down()
+    {
+        $this->forge->dropTable('item');
+    }
+}
