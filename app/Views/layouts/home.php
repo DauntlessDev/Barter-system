0037feeadb593a138c4b5a134612ad5e647f724d
DauntlessDev <braveleuterio.dauntlessdev@gmail.com>
2021-03-02 13:20:32 +0800

    added home layout

---
+++ app/Views/layouts/home.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <title>Trade-Off</title>
+</head>
+<body>
+    <nav>
+            <div>
+                <li class="nav">Home</li>
+            </div>
+            <div class="inline">
+                <li class="nav">Register</li>
+                <div class="spacer"></div>
+                <li class="login">Login</li>
+            </div>
+    </nav>
+
+    <div class="header">
+        <img src="assets/home/logo-sample.svg" alt="feature" class="logo">
+        <div class="search-bar">
+            <input class="search-input"></input>
+            <button class="search-button">Find</button>
+        </div>
+        <button class="sell-button">Sell</button>
+    </div>
+
+    <div class="wrapper">
+        <div class="feature">
+            <img src="assets/home/feature-sample.jpg"  alt="feature" class="feature-img">
+        </div>
+
+        <div class="explore">
+            <div class="explore-header">
+                <p>Explore Items</p>
+            </div>
+            <div class="category-container">
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+                <div class="category">
+                    <img src="assets/home/category-sample.png" alt="category 1">
+                    <p>Category 1</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="fresh-finds">
+            <div class="fresh-finds-header">
+                <p>Fresh Finds</p>
+            </div>
+            <div class="product-list">
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-header">
+                        <img src="assets/home/profile-pic-sample.jpg" alt="profile-pic" class="profile-pic">
+                        <div class="product-header-sub">
+                            <p>hannahlou888</p>
+                            <h6>1 minute ago</h6>
+                        </div>
+                    </div>
+                    <div class="main-img">
+                        <img src="assets/home/product-img-sample.jpg" alt="" class="product-img">
+                    </div>
+                    <div class="product-details">
+                        <p class="product-name">Carrot Headband</p>
+                        <p>PHP 800</p>
+                        <p>Swap isang carrot headband para sa isang apple headband...</p>
+                        <div class="new-tag">New</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="bottom-end"></div>
+    </div>
+
+    
+    <footer>Assigned to DauntlessDev - In Progress (Web Design)</footer>
+
+
+
+</body>
+</html>
