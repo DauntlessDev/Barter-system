4bde352f95ebe3abbd731976fd00037e88fb0551
Simperfy <28738855+Simperfy@users.noreply.github.com>
2021-03-03 21:36:38 +0800

    ADDED: GUI for messages

--- app/Views/components/message/index.php
+++ app/Views/components/message/index.php
@@ -1,4 +1,73 @@
 <div class="container">
-    <h1>Message ✉️</h1>
-    <p>Messages of <?= session()->get('user')['username'] ?? '' ?></p>
+    <div>
+        <h1>Messages ✉️</h1>
+    </div>
+
+    <div class="msg_container">
+        <div class="inbox_container">
+            <?php for ($i = 0; $i < 20; $i++) : ?>
+                <div class="inbox_card">
+                    <p>User <?= $i ?></p>
+                    <p>Hello world world world world world world world world world world</p>
+                </div>
+            <?php endfor; ?>
+        </div>
+        <div class="chatPanel_container">
+            <div class="recipient_container">User1</div>
+            <div class="content">
+                <?php for ($i = 0; $i < 20; $i++) : ?>
+                    <div class="chatBox_container sender">
+                        <div class="chatBox">
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsumLorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsumLorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsumLorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsum
+                            Lorem ipsumLorem ipsum
+                            <?= $i ?>
+                        </div>
+                    </div>
+                    <div class="chatBox_container receiver">
+                        <div class="chatBox">
+                            Hello World
+                            Hello World
+                            Hello World
+                            Hello World
+                            Hello World
+                            Hello World
+                            Hello World
+                            <?= $i ?>
+                        </div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+            <div class="msgTextArea_container">
+                <textarea class="msgTextArea" rows="3"></textarea>
+                <button class="btn send-btn">Send</button>
+            </div>
+        </div>
+    </div>
 </div>
