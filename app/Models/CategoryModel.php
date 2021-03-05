76636c0b77a7cd041dbefbc26c211d562ccd624b
Hezzz <49769113+Hezzz@users.noreply.github.com>
2021-03-06 01:54:54 +0800

    Update CategoryModel.php

--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -18,29 +18,15 @@ class CategoryModel extends Model implements ModelInterface
 
 	// protected $validationRules      = [];
 
+	
+	/* Query calls */
+    /* Functions to be used on controllers */
+	
+	
+	/* Create Methods */
+
 
 	/**
-     * Returns the first row from the `category` table given
-     * certain search conditions.
-     * 
-     * @param array $search_values
-     * Example: `$search_values = ['category_name' => 'fashion', ...]`
-     * 
-     */
-	public function get($search_values){
-		return $this->where($search_values)
-                    ->first();
-	}
-
-	/**
-     * Returns all category names as an indexed array.
-     * 
-     */
-    public function getAll($search_values = null, $limit = 0, $offset = 0){
-        return $this->findColumn('category_name');
-    }
-
-    /**
      * Creates new category into the database.
      * 
      *  @param array $data data of the category to be inserted.
@@ -50,6 +36,47 @@ class CategoryModel extends Model implements ModelInterface
     public function create($data){
         return $this->insert($data, false);
     }
+
+
+	/* Retrieve Methods */
+
+	/**
+     * Returns rows from the `category` table as an array, given
+     * a certain limit, otherwise returns all.
+	 * 
+     */
+    public function get($search_values = null, $limit = 0, $offset = 0){
+        return $this->findAll($limit, $offset);
+    }
+
+
+	/**
+	 * Gets the all the category names given a list of `category_id`s,
+	 * otherwise returns all category names from the table;
+	 * (Useful after getting the `category_id`s of an item,
+	 * after interaction with the `item_listing` table).
+	 * 
+	 * @param array $category_ids list of `category_id`s.
+	 * @param string $order alphabetical order of category names.
+	 * 
+	 */
+	public function getCategoryNames($category_ids = null, $order = 'asc'){
+		if($category_ids){
+			$builder = $this->builder();
+			return $builder->select('category_name')
+					->whereIn('category_id', $category_ids)
+					->orderBy($order)
+					->get()
+					->getResultArray();
+		}
+		else {
+			return $this->orderBy('category_name', $order)
+						->findColumn('category_name');				
+		}
+	}
+
+
+	/* Update Methods */
 
 
 	/**
