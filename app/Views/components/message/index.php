7653cdcf357b1d905bfecbdcb2dc66ed2527f7f2
Simperfy <28738855+Simperfy@users.noreply.github.com>
2021-03-08 14:26:05 +0800

    UPDATE: added javascript for dynamic chat

--- app/Views/components/message/index.php
+++ app/Views/components/message/index.php
@@ -5,64 +5,12 @@
 
     <div class="msg_container">
         <div class="inbox_container">
-            <?php for ($i = 0; $i < 20; $i++) : ?>
-                <div class="inbox_card">
-                    <p>User <?= $i ?></p>
-                    <p>Hello world world world world world world world world world world</p>
-                </div>
-            <?php endfor; ?>
+            <?php // inboxes ?>
         </div>
         <div class="chatPanel_container">
-            <div class="recipient_container">User1</div>
+            <div class="recipient_container">Select Recipient to load messages</div>
             <div class="content">
-                <?php for ($i = 0; $i < 20; $i++) : ?>
-                    <div class="chatBox_container sender">
-                        <div class="chatBox">
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsumLorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsumLorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsumLorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsum
-                            Lorem ipsumLorem ipsum
-                            <?= $i ?>
-                        </div>
-                    </div>
-                    <div class="chatBox_container receiver">
-                        <div class="chatBox">
-                            Hello World
-                            Hello World
-                            Hello World
-                            Hello World
-                            Hello World
-                            Hello World
-                            Hello World
-                            <?= $i ?>
-                        </div>
-                    </div>
-                <?php endfor; ?>
+                <?php // chats ?>
             </div>
             <div class="msgTextArea_container">
                 <textarea class="msgTextArea" rows="3"></textarea>
@@ -71,3 +19,5 @@
         </div>
     </div>
 </div>
+
+<?= $this->include('components/message/script') ?>
---
+++ public/js/messages.js
@@ -0,0 +1,122 @@
+const chatPanelContent = document.querySelector('.chatPanel_container .content');
+chatPanelContent.scrollTop = chatPanelContent.scrollHeight
+
+const sendBtn = document.querySelector('.send-btn');
+
+sendBtn.addEventListener('click', onSendBtnClick);
+
+function onSendBtnClick(e) {
+    const msgTextArea = document.querySelector('.msgTextArea');
+
+    sendChat(msgTextArea.value);
+    msgTextArea.value = '';
+}
+
+function receiveChat(msg) {
+    drawChat(msg, false);
+}
+
+function sendChat(msg) {
+    if (msg === '') return;
+
+    drawChat(msg);
+}
+
+function drawChat(msg, isSender = true) {
+    const chatBoxContainerDiv = createChatBoxDiv(msg, isSender);
+    chatPanelContent.appendChild(chatBoxContainerDiv);
+    chatPanelContent.scrollTop = chatPanelContent.scrollHeight
+}
+
+function createChatBoxDiv(msg, isSender = true) {
+    const chatBoxContainerDiv = document.createElement("div");
+    chatBoxContainerDiv.classList.add('chatBox_container');
+    chatBoxContainerDiv.classList.add(isSender ? 'sender' : 'receiver');
+
+    const chatBoxDiv = document.createElement("div");
+    chatBoxDiv.classList.add('chatBox');
+
+    const chat = document.createTextNode(msg);
+
+    chatBoxDiv.appendChild(chat);
+    chatBoxContainerDiv.appendChild(chatBoxDiv);
+
+    return chatBoxContainerDiv;
+}
+
+function setRecipient(username) {
+    document.querySelector('.recipient_container').textContent = username;
+}
+
+function APIManager() {
+    return {
+        getMessageHistoryWith(recipientUsername) {
+            // call api to get message history & load them all here
+            const data = [];
+            const username = window.username;
+
+            for (let i = 0; i < 20; i += 2) {
+                data.push({ username: username, content: 'lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor', timestamp: i });
+                data.push({ username: recipientUsername, content: 'dolor enet alrut dolor enet alrut dolor enet alrut', timestamp: i+1 });
+            }
+
+            return data;
+        }
+    }
+}
+
+const apiManager = APIManager();
+
+function loadMessages(recipientUsername) {
+    const messageHistory = apiManager.getMessageHistoryWith(recipientUsername);
+
+    for (const message of messageHistory) {
+        if (message.username !== window.username) receiveChat(message.content)
+        else sendChat(message.content)
+    }
+}
+
+const inbox = inboxManager();
+inbox.load();
+
+function inboxManager() {
+    function onInboxClick(e) {
+        const inboxCard = e.target.closest('.inbox_card');
+
+        const recipientUsername = inboxCard.dataset.username;
+
+        setRecipient(recipientUsername);
+        loadMessages(recipientUsername);
+    }
+
+    return {
+        load() {
+            const inboxContainer = document.querySelector('.inbox_container');
+
+            for (let i = 0; i < 20; i++) {
+                const user = `User${i}`;
+                const inbox = this.createInboxCard(user, 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ');
+
+                inbox.addEventListener('click', onInboxClick, false);
+                inbox.setAttribute('data-username', user)
+
+                inboxContainer.appendChild(inbox);
+            }
+        },
+        createInboxCard(recipientUsername, lastMsg) {
+            const chatBoxContainerDiv = document.createElement("div");
+            chatBoxContainerDiv.classList.add('inbox_card');
+
+            const userEl = document.createElement("p");
+            userEl.textContent = recipientUsername;
+
+            const lastMsgEl = document.createElement("p");
+            lastMsgEl.textContent = lastMsg;
+
+            chatBoxContainerDiv.appendChild(userEl);
+            chatBoxContainerDiv.appendChild(lastMsgEl);
+
+            return chatBoxContainerDiv;
+        }
+    };
+}
