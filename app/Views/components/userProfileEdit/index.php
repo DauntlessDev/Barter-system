00ab31355753ff04cdaa53aee412bddd4735149f
Brielle Romero <romerobrielle@gmail.com>
2021-03-10 21:58:30 +0800

    Updated the front end of the edit profile page

--- app/Views/components/userProfileEdit/index.php
+++ app/Views/components/userProfileEdit/index.php
@@ -1,7 +1,36 @@
-<div class="container">
-    <h1>Edit Profile ⚙️</h1>
-    <form class="form" action="<?= route_to('userProfileEdit') ?>" method="POST" id="editProfile-form">
-        <?= $this->include('components/partials/_userFields.php') ?>
-        <button class="btn btn-primary" type="submit" form="editProfile-form" value="submit">Edit Profile</button>
-    </form>
+<div class="ep-container">
+    <div class="ep-sidebar">
+        <div class="ep-sidebar-links">
+            <a href="" class="ep-side-link-active">Edit Profile</a>
+            <a href="" class="ep-side-link">Messages</a>
+        </div>
+    </div>
+
+    <div class="ep-content">
+        <div class="ep-content-box">
+            <h1>Edit Profile</h1>
+            <h3>Profile photo</h3>
+
+            <div class="ep-container-pp">
+                <img class="fit" src="img/sana.jpg">
+
+                <div class="ep-container-info">
+                <p>Clear frontal face photos are an important way for buyers and sellers to learn about each other. <b><i>Change this</i></b>.</p>
+                
+                <input class="ep-upload-pp" type="file" name="photo_url" id="photo_url" value="" autocomplete="off">
+                </div>
+            </div>
+
+            <form class="ep-box" action="<?= route_to('userProfileEdit') ?>" method="POST" id="editProfile-form">
+                <?= $this->include('components/partials/_userFields.php') ?>
+                <!-- <button class="button" type="submit" form="editProfile-form" value="submit">Edit Profile</button> -->
+            </form>
+
+            <div class="ep-divider"></div>
+            <div class="user-box">
+                <button class="button" type="submit" form="editProfile-form" value="submit">Save changes</button>
+            </div>
+        </div>
+    </div>
+
 </div>
