13f83defb47c6a7caad3be066dcd975d6be7a3be
Simperfy <28738855+Simperfy@users.noreply.github.com>
2021-03-11 19:04:49 +0800

    ADDED: more items in itemSeeder

--- app/Database/Seeds/ItemSeeder.php
+++ app/Database/Seeds/ItemSeeder.php
@@ -4,6 +4,7 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 use App\Models\ItemModel;
+use App\Models\CategoryModel;
 use Exception;
 use Faker\Factory;
 
@@ -12,22 +13,27 @@ class ItemSeeder extends Seeder
 	public function run()
 	{
 		$itemModel = new ItemModel();
+		$categoryModel = new CategoryModel();
 
 		$faker = Factory::create();
 
-		for ($i = 0; $i < 10; $i++) {
-			$data = [
-				'poster_uid'   => 1,
-				'item_name'    => "item$i",
-				'photo_url'    => 'https://via.placeholder.com/300',
-				'avail_status' => 'available',
-				'desc_title'   => $faker->word,
-				'desc_content' => $faker->text,
-			];
+		$categories = $categoryModel->findAll();
 
-			// check for validation error
-			if ($itemModel->create($data, [rand(1, 3), rand(1, 3)]) === false) {
-				throw new Exception('Error while inserting using ItemModel|'.implode('|', $itemModel->errors()));
+		foreach($categories as $category) {
+			for ($i = 0; $i < rand(1, 5); $i++) {
+				$data = [
+					'poster_uid'   => rand(1, 10),
+					'item_name'    => "item".$faker->word,
+					'photo_url'    => 'https://via.placeholder.com/300',
+					'avail_status' => 'available',
+					'desc_title'   => $faker->word,
+					'desc_content' => $faker->text,
+				];
+
+				// check for validation error
+				if ($itemModel->create($data, [$category['category_id'], rand(1, 10)]) === false) {
+					throw new Exception('Error while inserting using ItemModel|'.implode('|', $itemModel->errors()));
+				}
 			}
 		}
 	}
