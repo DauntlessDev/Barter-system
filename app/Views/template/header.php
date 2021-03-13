8858da4f709c948ecd52d21de92ed86fdaa19bf3
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2021-03-13 16:44:55 +0800

    REMOVED: var item_name in header

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -1,9 +1,8 @@
-<?php $item_name = ""; ?>
 <div class="header" >
     <img src="<?= base_url('assets/home/logo.png') ?>" alt="feature" class="logo">
     <form method="GET" action="<?php echo base_url("result");?>">
         <div class="search-bar">
-            <input type="text" class="search-input" name="item_name" id="item_name" value="<?php echo $item_name;?>">
+            <input type="text" class="search-input" name="item_name" id="item_name">
             <input type="submit" id="search" name="search" value="Search" class="search-button" >
         </div>
     </form> 
