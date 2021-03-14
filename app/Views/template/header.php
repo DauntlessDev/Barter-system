1bc36c5002438f3b032f11f863e243b328c1fb0c
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2021-03-14 23:04:05 +0800

    UPDATED: placeholder in search+categ-icon smaller

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -2,7 +2,7 @@
     <img src="<?= base_url('assets/home/logo.png') ?>" alt="feature" class="logo">
     <form method="GET" action="<?= route_to("result");?>">
         <div class="search-bar">
-            <input type="text" class="search-input" name="item_name" id="item_name">
+            <input placeholder=" Search for an item" type="text" class="search-input" name="item_name" id="item_name">
             <button type="submit" id="search" name="search" class="btn btn-success search-button">
                 <div class="fa fa-search icon-large "></div> 
             </button>
