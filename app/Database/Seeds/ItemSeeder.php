8b07c3e2c215868be2da3cbef69e0410fb78748b
Hezzz <49769113+Hezzz@users.noreply.github.com>
2021-03-28 21:34:30 +0800

    Updated Seeders
    - populated new user and item data

--- app/Database/Seeds/ItemSeeder.php
+++ app/Database/Seeds/ItemSeeder.php
@@ -13,30 +13,182 @@ class ItemSeeder extends Seeder
 	public function run()
 	{
 		$itemModel = new ItemModel();
-		$categoryModel = new CategoryModel();
-
 		$faker = Factory::create();
 
-		$categories = $categoryModel->findAll();
-		$counter = 0;
+		$itemData = [
+			[
+				'item_name' => "Nintendo Switch",
+				'category_ids' => [ 1, 2 ]
+			],
+			[
+				'item_name'    => "Yamaha DGX-660 88-key Piano",
+				'category_ids' => [ 2, 10 ]
+			],
+			[
+				'item_name'    => "Little Tikes Easy Score Basketball Set, Blue, 3 Balls",
+				'category_ids' => [ 2, 8 ]
+			],
+			[
+				'item_name'    => "Aqua 4-in-1 Monterey Hammock Inflatable Pool",
+				'category_ids' => [ 2, 8 ]
+			],
+			[
+				'item_name'    => "TOMY John Deere Sandbox Vehicle (2 Pack)",
+				'category_ids' => [ 2, 8 ]
+			],
+			[
+				'item_name'    => "Nerf N-Strike Elite Disruptor",
+				'category_ids' => [ 2, 8 ]
+			],
+			[
+				'item_name'    => "Water Sports 81055-7 Swim Thru Rings Assorted Pack",
+				'category_ids' => [ 2, 8 ]
+			],
+			[
+				'item_name'    => "BANZAI Bump N Bounce Body Bumpers N",
+				'category_ids' => [ 2, 8 ]
+			],
+			[
+				'item_name'    => "Gazillion Bubbles Hurricane Machine",
+				'category_ids' => [ 2, 8 ]
+			],
+			[
+				'item_name'    => "Fire 7 tablet (7\" display, 16 GB)",
+				'category_ids' => [ 3, 6, 9 ]
+			],
+			[
+				'item_name'    => "Lenovo Chromebook Flex 5 13\" Laptop",
+				'category_ids' => [ 3, 6, 9 ]
+			],
+			[
+				'item_name'    => "Expanding Organizer File Folder, Letter Size",
+				'category_ids' => [ 6, 9 ]
+			],
+			[
+				'item_name'    => "Matein Travel Laptop Backpack",
+				'category_ids' => [ 6, 9 ]
+			],
+			[
+				'item_name'    => "Tombow Fudenosuke Brush Pens (2-Pack)",
+				'category_ids' => [ 6, 9 ]
+			],
+			[
+				'item_name'    => "Furinno Pasir 3-Tier Open Shelf, Espresso",
+				'category_ids' => [ 6, 9 ]
+			],
+			[
+				'item_name'    => "X-ACTO School Pro Classroom Electric Pencil Sharpener",
+				'category_ids' => [ 6, 9 ]
+			],
+			[
+				'item_name'    => "Apple EarPods with Lightning Connector - White",
+				'category_ids' => [ 2, 3, 4 ]
+			],
+			[
+				'item_name'    => "Victrola Vintage 3-Speed Bluetooth Portable Suitcase Record Player with Built-in Speakers",
+				'category_ids' => [ 2, 3, 5 ]
+			],
+			[
+				'item_name'    => "Michael Kors Men's Slim Runway Stainless Steel Quartz Watch",
+				'category_ids' => [10]
+			],
+			[
+				'item_name'    => "Nintendo Switch Pro Controller",
+				'category_ids' => [ 2, 3 ]
+			],
+			[
+				'item_name'    => "Columbia Men's Steens Mountain Full Zip 2.0 Fleece",
+				'category_ids' => [1]
+			],
+			[
+				'item_name'    => "Columbia Men's Newton Ridge Plus II Waterproof Hiking Boot",
+				'category_ids' => [1]
+			],
+			[
+				'item_name'    => "Columbia mens Ascender Softshell Front-zip Jacket",
+				'category_ids' => [1]
+			],
+			[
+				'item_name'    => "Lysol Laundry Sanitizer Additive",
+				'category_ids' => [5]
+			],
+			[
+				'item_name'    => "Beckham Hotel Collection Bed Pillows for Sleeping - Queen Size",
+				'category_ids' => [5]
+			],
+			[
+				'item_name'    => "Sofa bed fabric double size bed",
+				'category_ids' => [5]
+			],
+			[
+				'item_name'    => "Pull&Bear Drape Top",
+				'category_ids' => [1]
+			],
+			[
+				'item_name'    => "Naruto funko pop and pikachu",
+				'category_ids' => [2, 7]
+			],
+			[
+				'item_name'    => "Xyclone Bike Foldable",
+				'category_ids' => [7]
+			],
+			[
+				'item_name'    => "Basketball Board Set",
+				'category_ids' => [10, 7]
+			],
+			[
+				'item_name'    => "Bennet Mountain bike",
+				'category_ids' => [10, 7]
+			],
+			[
+				'item_name'    => "Adidas Starlancer V Soccer Ball",
+				'category_ids' => [2, 7]
+			],
+			[
+				'item_name'    => "Sewing machine",
+				'category_ids' => [4, 5]
+			],
+			[
+				'item_name'    => "Portable aircon",
+				'category_ids' => [4, 5]
+			],
+			[
+				'item_name'    => "Panasonic Outlet",
+				'category_ids' => [4, 5]
+			],
+			[
+				'item_name'    => "MXQ PRO TV blackbox",
+				'category_ids' => [4, 5]
+			],
+			[
+				'item_name'    => "LG Aircon ",
+				'category_ids' => [4, 5]
+			],
+			[
+				'item_name'    => "Dyson style digital V6",
+				'category_ids' => [4, 5]
+			],
+			[
+				'item_name'    => "Huawei GT2 Watch",
+				'category_ids' => [3]
+			],
+		];
 
-		foreach($categories as $category) {
-			for ($i = 0; $i < rand(1, 5); $i++) {
-				$counter += 1;
-				$data = [
-					'poster_uid'   => rand(1, 10),
-					'item_name'    => "item ".$counter,
-					'photo_url'    => 'images/default/product.jpg',
+		foreach($itemData as $item){
+			$product_num = rand(1, 3);
+			$data = [
+					'poster_uid'   => rand(1, 15),
+					'item_name'    => $item['item_name'],
+					'photo_url'    => "images/default/product$product_num.jpg",
 					'avail_status' => 'available',
-					'desc_title'   => "description".$counter,
+					'desc_title'   => $faker->word,
 					'desc_content' => $faker->text,
 				];
 
-				// check for validation error
-				if ($itemModel->create($data, [$category['category_id'], rand(1, 10)]) === false) {
+			if ($itemModel->create($data, $item['category_ids']) === false) {
 					throw new Exception('Error while inserting using ItemModel|'.implode('|', $itemModel->errors()));
-				}
 			}
 		}
+		
 	}
 }
--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -5,6 +5,7 @@ namespace App\Database\Seeds;
 use CodeIgniter\Database\Seeder;
 use App\Models\UserModel;
 use Exception;
+use Faker\Factory;
 
 // https://www.codeigniter.com/user_guide/dbmgmt/index.html
 // Don't forget to call in BaseSeeder.php
@@ -13,23 +14,134 @@ class UserSeeder extends Seeder
 	public function run()
 	{
 		$userModel = new UserModel();
+		$faker = Factory::create();
 
-		for ($i = 0; $i < 10; $i++) {
-			$randomLowerCaseLetters = chr(rand(97,122)) . chr(rand(97,122)) . chr(rand(97,122));
+		$userdata = [
+			[   'username' => "johnrandy",
+				'first_name' => "Nicolai Randy",
+				'last_name' => "Castro",
+				'address' => "Manila PH",
+				'contact_details' => "09748271941",
+			],
+			[   'username' => "dariorusty",
+				'first_name' => "Dario Rusty",
+				'last_name' => "Lovel",
+				'address' => "Cebu PH",
+				'contact_details' => "09820561012",
+			],
+			[   'username' => "maryann",
+				'first_name' => "Mary Ann",
+				'last_name' => "Bardsley",
+				'address' => "Davao PH",
+				'contact_details' => "09647284081",
+			],
+			[   'username' => "adelina",
+				'first_name' => "Adelina Maximiliano",
+				'last_name' => "Ashley",
+				'address' => "Cebu PH",
+				'contact_details' => "09747691748",
+			],
+			[   'username' => "delphiawardell",
+				'first_name' => "Delphia Wardell",
+				'last_name' => "Dickenson",
+				'address' => "Pampanga PH",
+				'contact_details' => "09127893842",
+			],
+			[   'username' => "ionabradford",
+				'first_name' => "Iona Bradford",
+				'last_name' => "Reed",
+				'address' => "Sulu PH",
+				'contact_details' => "09875392834",
+			],
+			[   'username' => "marissavergil",
+				'first_name' => "Marissa Vergil",
+				'last_name' => "Langdon",
+				'address' => "Capiz PH",
+				'contact_details' => "0937478192",
+			],
+			[   'username' => "marimiku",
+				'first_name' => "Mari Miku",
+				'last_name' => "Sato",
+				'address' => "Iloilo PH",
+				'contact_details' => "09950243892",
+			],
+			[   'username' => "laurenmary",
+				'first_name' => "Lauren Mary",
+				'last_name' => "Roach",
+				'address' => "Manila PH",
+				'contact_details' => "09387391092",
+			],
+			[   'username' => "darcie11",
+				'first_name' => "Darcie Aranzazu",
+				'last_name' => "Hubert",
+				'address' => "Makati PH",
+				'contact_details' => "09919239472",
+			],
+			[   'username' => "jeffreynicodemo",
+				'first_name' => "Jeffrey Nicodemo",
+				'last_name' => "Eliot",
+				'address' => "Pasig PH",
+				'contact_details' => "09112387342",
+			],
+			[   'username' => "daphnekeegan",
+				'first_name' => "Daphne Keegan",
+				'last_name' => "Ericson",
+				'address' => "Marikina PH",
+				'contact_details' => "09464738288",
+			],
+			[   'username' => "cullenismael",
+				'first_name' => "Cullen Ismael",
+				'last_name' => "Tennison",
+				'address' => "Pasay PH",
+				'contact_details' => "09994428340",
+			],
+			[   'username' => "jenniferlucian",
+				'first_name' => "Jennifer Lucian",
+				'last_name' => "Colbert",
+				'address' => "Makati PH",
+				'contact_details' => "09119298374",
+			],
+			[   'username' => "mozelleethan",
+				'first_name' => "Mozelle Ethan",
+				'last_name' => "Marino",
+				'address' => "Cebu PH",
+				'contact_details' => "09128599342",
+			],
+		];
+
+		foreach($userdata as $user){
+			$random_num = rand(1, 3);
 
 			$data = [
-				'username' => "user$i",
+				'username' => $user['username'].$random_num,
 				'password' => '12345678',
-				'first_name' => "john $randomLowerCaseLetters",
-				'last_name' => "doe $randomLowerCaseLetters",
-				'address' => "PH",
-				'contact_details' => "09999999999",
+				'first_name' => $user['first_name'],
+				'last_name' => $user['last_name'],
+				'address' => $user['address'],
+				'photo_url' => "images/default/profile$random_num.jpg",
+				'contact_details' => $user['contact_details']
 			];
 
 			// check for validation error
 			if ($userModel->create($data) === false) {
 				throw new Exception('Error while inserting using UserModel|'.implode('|', $userModel->errors()));
 			}
+
+		}
+
+		$randomLowerCaseLetters = chr(rand(97,122)) . chr(rand(97,122)) . chr(rand(97,122));
+		$data = [
+			'username' => "user0",
+			'password' => '12345678',
+			'first_name' => "john $randomLowerCaseLetters",
+			'last_name' => "doe $randomLowerCaseLetters",
+			'address' => "PH",
+			'photo_url' => "images/default/profile1.jpg",
+			'contact_details' => "09999999999",
+		];
+
+		if ($userModel->create($data) === false) {
+			throw new Exception('Error while inserting using UserModel|'.implode('|', $userModel->errors()));
 		}
 	}
 }
